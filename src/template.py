afe391250695c7b870d4cc94819d31ca52f56c59
Domia Dev <domia@domia.dev>
2023-12-03 13:32:06 -0500

    template

---
+++ src/template.py
@@ -0,0 +1,29 @@
+import re
+
+def part1(input: str) -> int:
+    return 0
+
+def part2(input: str) -> int:
+    return 0
+        
+def main():
+    print(f"=== Part 1 ===")
+    print(f"example: {part1(ex1)}")
+    assert 0 == part1(ex1)
+    print(f"actual:  {part1(input)}")
+    assert 0 == part1(input)
+
+    print(f"=== Part 2 ===")
+    print(f"example: {part2(ex1)}")
+    assert 0 == part2(ex1)
+    print(f"actual:  {part2(input)}")
+    assert 0 == part2(input)
+
+ex1 = '''
+'''
+
+input = '''
+'''
+
+if __name__ == "__main__":
+    main()
